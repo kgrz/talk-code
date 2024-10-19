603cb6894a769c7a0a95c826f4340224736cdaa7
Kashyap Kondamudi <kashyap.kmbc@gmail.com>
2024-10-19 12:18:55 +0530

    Latest

--- story-1/server-2.mjs
+++ story-1/server-2.mjs
@@ -11,50 +11,83 @@ const popJsons = {
 	china: chjson,
 }
 
-const popJsonsBloated = ((jsons) => {
+const bloat = jsons => {
 	let final = {};
 
 	for (let key in jsons) {
 		let json = jsons[key];
-		final[key] = { main: json, bloat: Array(100).fill(json) };
+		final[key] = { main: json, bloat: Array(60).fill(json) };
 	}
 
 	return final;
-})(popJsons);
+}
 
-const popJsonsStringified = ((jsons) => {
+const normal = jsons => {
+	let final = {};
+
+	for (let key in jsons) {
+		let json = jsons[key];
+		final[key] = { main: json };
+	}
+
+	return final;
+}
+
+const stringify = jsons => {
 	let final = {};
 	for (let key in jsons) {
 		final[key] = JSON.stringify(jsons[key]);
 	}
 
 	return final;
-})(popJsonsBloated);
+}
 
-const popJsonsGzipped = ((jsons) => {
+const zip = jsons => {
 	let final = {};
 	for (let key in jsons) {
 		final[key] = gzipSync(jsons[key]);
 	}
 
 	return final;
-})(popJsonsStringified);
+}
+
+const popJsonsBloated = bloat(popJsons);
+const popJsonsNormal = normal(popJsons);
+const popJsonsStringified = stringify(popJsonsBloated);
+const popJsonsGzipped = zip(popJsonsStringified);
+
+const popJsonsStringifiedN = stringify(popJsonsNormal);
+const popJsonsGzippedN = zip(popJsonsStringifiedN);
 
 
 app.post('/pop/:country', async (req, res) => {
-	const timeout = Math.floor(Math.random() * 100)
-	const random = Math.floor(Math.random() * 10);
-	await setTimeoutPromise(timeout);
+	res.setHeader('Content-Type', 'application/json');
 
 	let jsons = popJsonsStringified;
+	if (req.query.nobloat) {
+		jsons = popJsonsStringifiedN;
+	}
+
 	const encoding = req.headers['accept-encoding'];
 	if (encoding === 'gzip') {
 		res.setHeader('Content-Encoding', 'gzip');
-		jsons = popJsonsGzipped;
+
+		if (req.query.nobloat) {
+			jsons = popJsonsStringifiedN;
+		} else {
+			jsons = popJsonsGzipped;
+		}
 	}
-	res.setHeader('Content-Type', 'application/json');
 
 	let response = jsons[req.params.country] ?? {};
+
+	if (req.query.nobloat) {
+		return res.send(response);
+	}
+
+	const timeout = Math.floor(Math.random() * 100)
+	const random = Math.floor(Math.random() * 10);
+	await setTimeoutPromise(timeout);
 
 	return res.send(response);
 });
